e6dd8ef0ac530acb264a8634c58c59f1e15e4492
andn <andn@KBN-AFN-W72>
2016-09-08 12:53:35 +0200

    Add project files.

---
+++ Namesearch/Project.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Namesearch
+{
+    class Project
+    {
+        static int Main(string[] args)
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t', ';'};
+
+            //string path_navne = @"C:\Private\Data\FE\Opgave\danske_drengenavne.txt";
+            string path_navne = @"C:\Private\Data\FE\Opgave\drengenavne_test.txt";
+            string navne_liste = File.ReadAllText(path_navne);
+            string[] navne = navne_liste.Split(delimiterChars);
+            uint[] navne_hash = new uint[(int)navne.Length];
+
+            //string path_tekst = @"C:\Private\Data\FE\Opgave\sample_tekst.txt";
+            string path_tekst = @"C:\Private\Data\FE\Opgave\sample_tekst_test.txt";
+            string tekst = File.ReadAllText(path_tekst);
+            string[] ord = tekst.Split(delimiterChars);
+            uint[] tekst_hash = new uint[(int)ord.Length];
+
+            foreach (string s in navne)
+                Console.WriteLine("Navn: {0}", s);
+
+            foreach (string s in ord)
+                Console.WriteLine("Ord: {0}", s);
+
+
+            // Hash navne og ord
+            for (int n = 0; n < ord.Length; n++)
+            {
+                tekst_hash[n] = SearchText.hash(ord[n]);
+            }
+
+            for (int n = 0; n < navne.Length; n++)
+            {
+                navne_hash[n] = SearchText.hash( navne[n] );
+            }
+
+            //for (int i = 0; i < 6; i++)
+            //    Console.WriteLine("Navn: {0}", navne_hash[i]);
+
+            //Array.Sort(navne_hash);
+            foreach (uint n in navne_hash)
+                Console.WriteLine("Sorted hash, navne: {0}", n);
+
+            //Array.Sort(navne_hash);
+            foreach (uint n in tekst_hash)
+                Console.WriteLine("Sorted hash, tekst: {0}", n);
+
+            uint[,] fundne_navne = new uint[(int)navne.Length, 2];
+            uint navne_idx = 0;
+            uint matches = 0;
+            uint unikke_navne = 0;
+
+            for (uint m = 0; m < navne_hash.Length; m++)
+            {
+                for (uint n = 0; n < tekst_hash.Length; n++)
+                {
+                    if (navne_hash[m] == tekst_hash[n])
+                    {
+                        matches += 1;
+                        //if (navne_idx != m) // New name found!
+                        //{
+                        //    navne_idx = m;
+                        //    unikke_navne += 1;
+                        //}
+                        //fundne_navne[unikke_navne, 1] = m;
+                        //fundne_navne[unikke_navne, 2] = matches;                        
+                    }
+                }
+            }
+
+            Console.WriteLine("Matches: {0}", matches);
+
+            //for (uint n = 0; n < unikke_navne; n++)
+            //    Console.WriteLine("Navn (Hash): {0}, Antal forekomster: {1}", fundne_navne[n,1], fundne_navne[n,2]);
+
+            Console.ReadKey();
+
+            return 0;
+        }
+    }
+}
---
+++ Namesearch/SearchText.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+public class SearchText
+{
+    const uint PRIME_BASE = 257;
+    const uint PRIME_MOD = 1000000007;
+
+    public static int[] rabinKarbSearch(string A, string B)
+    {
+        List<int> retVal = new List<int>();
+        ulong siga = 0;
+        ulong sigb = 0;
+        ulong Q = 100007;
+        ulong D = 256;
+
+        for (int i = 0; i < B.Length; ++i)
+        {
+            siga = (siga * D + (ulong)A[i]) % Q;
+            sigb = (sigb * D + (ulong)B[i]) % Q;
+        }
+
+        if (siga == sigb)
+            retVal.Add(0);
+
+        ulong pow = 1;
+
+        for (int k = 1; k <= B.Length - 1; ++k)
+            pow = (pow * D) % Q;
+
+        for (int j = 1; j <= A.Length - B.Length; ++j)
+        {
+            siga = (siga + Q - pow * (ulong)A[j - 1] % Q) % Q;
+            siga = (siga * D + (ulong)A[j + B.Length - 1]) % Q;
+
+            if (siga == sigb)
+                if (A.Substring(j, B.Length) == B)
+                    retVal.Add(j);
+        }
+
+        return retVal.ToArray();
+    }
+
+    public static uint hash( string key )
+    {
+        uint hashval = 0;
+
+        for ( int i = 0; i < key.Length; i++)
+        {
+            hashval = key[i] + PRIME_BASE * hashval;
+        }
+
+        return hashval % PRIME_MOD; 
+    }
+
+    public static uint hash_navne(string key)
+    {
+        uint hashval = 0;
+
+        for (int i = 2; i < key.Length - 2; i++)
+        {
+            hashval = key[i] + PRIME_BASE * hashval;
+        }
+
+        return hashval % PRIME_MOD;
+    }
+}
